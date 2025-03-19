843551583377bb68436f3459edd7317c9d491855
Anushka Gupta <anushka.gupta@ibm.com>
2025-03-19 17:08:31 +0530

    Dummy test website

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,18 +7,6 @@
     const sticky = ud_header.offsetTop;
     const logo = document.querySelector(".navbar-brand img");
 
-    if (window.pageYOffset > sticky) {
-      ud_header.classList.add("sticky");
-    } else {
-      ud_header.classList.remove("sticky");
-    }
-
-    // === logo change
-    if (ud_header.classList.contains("sticky")) {
-      logo.src = "assets/images/logo/logo-2.svg";
-    } else {
-      logo.src = "assets/images/logo/logo.svg";
-    }
 
     // show or hide the back-top-top button
     const backToTop = document.querySelector(".back-to-top");
